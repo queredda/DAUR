736f6c78676757c8d8d5adfd130e9efc2382eb42
Venamora <stevennamorarohasaraginapitu@mail.ugm.ac.id>
2024-09-09 09:49:21 +0700

    msg: fix ui login page

--- daur/Properties/Resources.Designer.cs
+++ daur/Properties/Resources.Designer.cs
@@ -59,5 +59,25 @@ namespace DAUR.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _01___Login {
+            get {
+                object obj = ResourceManager.GetObject("01 - Login", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _01___Login1 {
+            get {
+                object obj = ResourceManager.GetObject("01 - Login1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }

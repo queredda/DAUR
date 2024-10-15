465622a643955a7b8bcb4305ad8fc9976ffa4fd9
Venamora <stevennamorarohasaraginapitu@mail.ugm.ac.id>
2024-10-15 21:19:23 +0700

    Menambahkan signup page & move logic

---
+++ daur/loginPage.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace DAUR
+{
+    partial class loginPage
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginPage));
+            button1 = new Button();
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            button5 = new Button();
+            button4 = new Button();
+            label5 = new Label();
+            button3 = new Button();
+            label4 = new Label();
+            tbPassword = new TextBox();
+            button2 = new Button();
+            label3 = new Label();
+            label2 = new Label();
+            tbEmail = new TextBox();
+            label1 = new Label();
+            lblSign = new Label();
+            linkSign = new LinkLabel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(button1, "button1");
+            button1.BackColor = Color.Transparent;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.ForeColor = Color.Black;
+            button1.Name = "button1";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            button1.MouseEnter += button1_MouseEnter;
+            button1.MouseLeave += button1_MouseLeave;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(linkSign);
+            panel1.Controls.Add(lblSign);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(tbPassword);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(tbEmail);
+            panel1.Controls.Add(label1);
+            resources.ApplyResources(panel1, "panel1");
+            panel1.Name = "panel1";
+            panel1.Paint += panel1_Paint;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            resources.ApplyResources(pictureBox1, "pictureBox1");
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.TabStop = false;
+            // 
+            // button5
+            // 
+            resources.ApplyResources(button5, "button5");
+            button5.Name = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(button4, "button4");
+            button4.Name = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(label5, "label5");
+            label5.BackColor = Color.Transparent;
+            label5.Name = "label5";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(button3, "button3");
+            button3.Name = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(label4, "label4");
+            label4.BackColor = Color.Transparent;
+            label4.ForeColor = Color.FromArgb(66, 66, 66);
+            label4.Name = "label4";
+            // 
+            // tbPassword
+            // 
+            resources.ApplyResources(tbPassword, "tbPassword");
+            tbPassword.ForeColor = Color.Gray;
+            tbPassword.Name = "tbPassword";
+            tbPassword.UseSystemPasswordChar = true;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(66, 66, 66);
+            button2.FlatAppearance.BorderSize = 0;
+            resources.ApplyResources(button2, "button2");
+            button2.ForeColor = Color.White;
+            button2.Name = "button2";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(label3, "label3");
+            label3.BackColor = Color.Transparent;
+            label3.ForeColor = Color.FromArgb(66, 66, 66);
+            label3.Name = "label3";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            resources.ApplyResources(label2, "label2");
+            label2.BackColor = Color.Transparent;
+            label2.ForeColor = Color.FromArgb(66, 66, 66);
+            label2.Name = "label2";
+            // 
+            // tbEmail
+            // 
+            resources.ApplyResources(tbEmail, "tbEmail");
+            tbEmail.ForeColor = Color.Gray;
+            tbEmail.Name = "tbEmail";
+            tbEmail.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(label1, "label1");
+            label1.BackColor = Color.Transparent;
+            label1.ForeColor = Color.FromArgb(66, 66, 66);
+            label1.Name = "label1";
+            // 
+            // lblSign
+            // 
+            resources.ApplyResources(lblSign, "lblSign");
+            lblSign.BackColor = Color.Transparent;
+            lblSign.ForeColor = Color.FromArgb(66, 66, 66);
+            lblSign.Name = "lblSign";
+            // 
+            // linkSign
+            // 
+            resources.ApplyResources(linkSign, "linkSign");
+            linkSign.Name = "linkSign";
+            linkSign.TabStop = true;
+            linkSign.LinkClicked += linkSign_LinkClicked;
+            // 
+            // loginPage
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Silver;
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "loginPage";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Panel panel1;
+        private Button button2;
+        private Label label3;
+        private Label label2;
+        private TextBox tbEmail;
+        private Label label1;
+        private Label label4;
+        private TextBox tbPassword;
+        private Button button5;
+        private Button button4;
+        private Label label5;
+        private Button button3;
+        private PictureBox pictureBox1;
+        private Label lblSign;
+        private LinkLabel linkSign;
+    }
+}
---
+++ daur/signupPage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace DAUR
+{
+    public partial class signupPage : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        public signupPage()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void signupPage_Load(object sender, EventArgs e)
+        {
+            // Set round corners for controls
+            btn_SignUp.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, btn_SignUp.Width, btn_SignUp.Height, 35, 35));
+            tbName.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, tbName.Width, tbName.Height, 15, 15));
+            tbEmail.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, tbEmail.Width, tbEmail.Height, 15, 15));
+            tbPassword.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, tbPassword.Width, tbPassword.Height, 15, 15));
+            tbCP.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, tbCP.Width, tbCP.Height, 15, 15));
+            signupPnl.Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, signupPnl.Width, signupPnl.Height, 25, 25));
+
+        }
+
+        private void signupPnl_Paint(object sender, PaintEventArgs e)
+        {
+            signupPnl.BackColor = Color.FromArgb(120, 72, 207, 203);
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+            // Add signup logic here
+        }
+
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            button.BackColor = Color.Gainsboro;
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            button.BackColor = SystemColors.Control;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLog_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            OpenLogInPage();
+        }
+
+        private void OpenLogInPage()
+        {
+            loginPage loginForm = new loginPage();
+            loginForm.Show();
+            this.Hide();
+        }
+    }
+}

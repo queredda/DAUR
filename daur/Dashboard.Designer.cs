83222fe48907a13d1bebe4216549d33c6378f90f
Venamora <stevennamorarohasaraginapitu@mail.ugm.ac.id>
2024-11-07 13:13:22 +0700

    menambah page baru

---
+++ daur/Dashboard.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace DAUR
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Montserrat SemiBold", 13.7999992F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(154, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(205, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome, Tipen";
+            // 
+            // Dashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1366, 768);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1366, 768);
+            Name = "Dashboard";
+            Text = "Dashboard";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}

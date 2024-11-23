da09b8d49fc5a8e60022066fed40b22977fa4cc2
Venamora <stevennamorarohasaraginapitu@mail.ugm.ac.id>
2024-11-23 22:01:25 +0700

    fix: dashboard name

---
+++ daur/IndustriDashboard.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DAUR
+{
+    public partial class IndustriDashboard : Form
+    {
+        public IndustriDashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void newDashboard_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OpenSend()
+        {
+            send send = new send();
+            send.Show();
+            this.Close();
+        }
+        private void OpenSetting()
+        {
+            setting setting = new setting();
+            setting.Show();
+            this.Close();
+        }
+        private void OpenProfile()
+        {
+            Profile Profile = new Profile();
+            Profile.Show();
+            this.Close();
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            OpenSetting();
+        }
+
+        private void btnProfile_Click(object sender, EventArgs e)
+        {
+            OpenProfile();
+        }
+
+        private void btnSetting_Click(object sender, EventArgs e)
+        {
+            OpenSetting();
+        }
+    }
+}
